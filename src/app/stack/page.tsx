dd57ae020f5d597bb2ce39d7a351968b2bbde554
gibify <gibifyofficial@gmail.com>
2024-07-05 08:56:09 -0300

    stacks

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -14,73 +14,72 @@ import Link from "next/link"
 
 export default function Stacks() {
   return (
-    <div className="flex justify-center items-center gap-8">
-      <Link href="https://nextjs.org/" target="_blank">
-      <Card className="flex flex-col w-[180px] items-center justify-center border-2 border-[#0070F3] ">
-        <CardContent className="p-[30px] back">
-          <NextjsIcon/>
-        </CardContent>
-        <CardFooter>
-          <CardTitle className="text-[#0070F3]">Next</CardTitle>
-        </CardFooter>
-      </Card>
-      </Link>
-     
-      <Link href="https://react.dev/" target="_blank">
-        <Card className="flex flex-col w-[180px] items-center justify-center border-2 border-[#2AC7E3]">
-          <CardContent className="p-[30px]">
-            <ReactjsIcon/>
-          </CardContent>
-          <CardFooter>
-            <CardTitle className="text-[#2AC7E3]">React</CardTitle>
-          </CardFooter>
-        </Card>
-      </Link>
+    <div className="flex flex-wrap justify-center items-center gap-8">
+  <Link href="https://nextjs.org/" target="_blank">
+    <Card className="flex flex-col w-[150px] sm:w-[180px] items-center justify-center border-2 border-[#0070F3]">
+      <CardContent className="p-[20px] sm:p-[30px]">
+        <NextjsIcon />
+      </CardContent>
+      <CardFooter>
+        <CardTitle className="text-[#0070F3] text-lg">Next</CardTitle>
+      </CardFooter>
+    </Card>
+  </Link>
 
-      <Link href="https://nodejs.org/en" target="_blank">
-        <Card className="flex flex-col w-[180px] items-center justify-center border-2 border-[#8CC84B]">
-          <CardContent className="p-[30px]">
-            <NodejsIcon />
-          </CardContent>
-          <CardFooter>
-            <CardTitle className="text-[#8CC84B]">Node</CardTitle>
-          </CardFooter>
-        </Card>
-      </Link>
+  <Link href="https://react.dev/" target="_blank">
+    <Card className="flex flex-col w-[150px] sm:w-[180px] items-center justify-center border-2 border-[#2AC7E3]">
+      <CardContent className="p-[20px] sm:p-[30px]">
+        <ReactjsIcon />
+      </CardContent>
+      <CardFooter>
+        <CardTitle className="text-[#2AC7E3] text-lg">React</CardTitle>
+      </CardFooter>
+    </Card>
+  </Link>
 
-      <Link href="https://www.electronjs.org/" target="_blank">
-        <Card className="flex flex-col w-[180px] items-center justify-center border-2 border-[#A2ECFB]">
-          <CardContent className="p-[30px]">
-            <ElectronjsIcon />
-          </CardContent>
-          <CardFooter>
-            <CardTitle className="text-[#A2ECFB]">Electron</CardTitle>
-          </CardFooter>
-        </Card>
-      </Link>
+  <Link href="https://nodejs.org/en" target="_blank">
+    <Card className="flex flex-col w-[150px] sm:w-[180px] items-center justify-center border-2 border-[#8CC84B]">
+      <CardContent className="p-[20px] sm:p-[30px]">
+        <NodejsIcon />
+      </CardContent>
+      <CardFooter>
+        <CardTitle className="text-[#8CC84B] text-lg">Node</CardTitle>
+      </CardFooter>
+    </Card>
+  </Link>
 
-      <Link href="https://www.javascript.com/" target="_blank">
-        <Card className="flex flex-col w-[180px] items-center justify-center border-2 border-[#F7DF1E]">
-          <CardContent className="p-[30px]">
-            <JavascriptIcon />
-          </CardContent>
-          <CardFooter>
-            <CardTitle className="text-[#F7DF1E]">Javascript</CardTitle>
-          </CardFooter>
-        </Card>
-      </Link>
+  <Link href="https://www.electronjs.org/" target="_blank">
+    <Card className="flex flex-col w-[150px] sm:w-[180px] items-center justify-center border-2 border-[#A2ECFB]">
+      <CardContent className="p-[20px] sm:p-[30px]">
+        <ElectronjsIcon />
+      </CardContent>
+      <CardFooter>
+        <CardTitle className="text-[#A2ECFB] text-lg">Electron</CardTitle>
+      </CardFooter>
+    </Card>
+  </Link>
 
-          
-      <Link href="https://www.typescriptlang.org/" target="_blank">
-        <Card className="flex flex-col w-[180px] items-center justify-center border-2 border-[#3178C6]">
-          <CardContent className="p-[30px]">
-            <TypescriptIcon />
-          </CardContent>
-          <CardFooter>
-            <CardTitle className="text-[#3178C6]">Typescript</CardTitle>
-          </CardFooter>
-        </Card>
-      </Link>
-    </div>
+  <Link href="https://www.javascript.com/" target="_blank">
+    <Card className="flex flex-col w-[150px] sm:w-[180px] items-center justify-center border-2 border-[#F7DF1E]">
+      <CardContent className="p-[20px] sm:p-[30px]">
+        <JavascriptIcon />
+      </CardContent>
+      <CardFooter>
+        <CardTitle className="text-[#F7DF1E] text-lg">Javascript</CardTitle>
+      </CardFooter>
+    </Card>
+  </Link>
+
+  <Link href="https://www.typescriptlang.org/" target="_blank">
+    <Card className="flex flex-col w-[150px] sm:w-[180px] items-center justify-center border-2 border-[#3178C6]">
+      <CardContent className="p-[20px] sm:p-[30px]">
+        <TypescriptIcon />
+      </CardContent>
+      <CardFooter>
+        <CardTitle className="text-[#3178C6] text-lg">Typescript</CardTitle>
+      </CardFooter>
+    </Card>
+  </Link>
+</div>
   )
 }
