9223e9ead91ec67e22aec4eb328c7a1fcc9a746c
gibify <gibifyofficial@gmail.com>
2024-08-26 20:52:24 -0400

    adjusts

--- src/components/Portuguese/page.tsx
+++ src/components/Portuguese/page.tsx
@@ -8,18 +8,18 @@ export default function Portuguese() {
       <br /> Sou Evandro Gibicoski, conhecido como <code>Gibi</code>
     </h1>
     <p className="text-xl">
-      Como desenvolvedor de software, tenho uma paixão por criar soluções de software.
-      Minha experiência abrange tecnologias voltadas para a Web e aplicativos Desktop multiplataforma, permitindo-me adaptar em ambientes de desenvolvimento em constante mudança.
+    Como desenvolvedor de software, sou apaixonado por criar soluções inovadoras. 
+    Minha experiência abrange tecnologias voltadas para a Web e aplicativos Desktop multiplataforma, permitindo-me adaptar a ambientes de desenvolvimento em constante evolução.
     </p>
 
     < br />
     <p className="text-xl">
-      <code>Sou formado em Gestão da Tecnologia da Informação pela Faculdade Unicesumar de Maringá e Pós Graduado em Engenharia de Software pela Universidade Metropolitana de SP.</code>
+      <code>Sou formado em Gestão da Tecnologia da Informação pela Faculdade Unicesumar de Maringá e Pós-Graduado em Engenharia de Software pela Universidade Metropolitana de SP.</code>
     </p>
 
     < br />
     <p className="text-xl mb-10">
-      Atualmente trabalho como <strong>Desenvolvedor Especialista</strong> no <a href="https://www.grupoboticario.com.br/" target="_blank" className="text-[#78866B] font-semibold">Grupo Boticário</a>.
+      Atualmente, trabalho como <strong>Desenvolvedor Especialista</strong> no <a href="https://www.grupoboticario.com.br/" target="_blank" className="text-[#78866B] font-semibold">Grupo Boticário</a>.
     </p>
 
     <Link href="/contact" className="text-[#78866B] border border-[#78866B] py-4 px-6 rounded-full font-semibold">Entre em contato</Link>
--- src/components/contact-page/contact-form-in-portuguese.tsx
+++ src/components/contact-page/contact-form-in-portuguese.tsx
@@ -5,10 +5,10 @@ export function ConatctFormInPortuguese() {
     <form className="flex  flex-col gap-6 max-w-[500px] mt-12 w-full p-4" >
       <h1 className="text-2xl">Entre em Contato</h1>
       <p className="text-md">
-        Envia uma mensagem e deixa nos saber sobre suas dúvidas e sugestões.
+        Envia-me uma mensagem.
       </p>
       <div className="flex flex-col gap-2 w-full">
-        <label htmlFor="">Email</label>
+        <label htmlFor="">E-mail</label>
         <input type="text" placeholder="Seu melhor e-mail" className="w-full px-4 py-2 rounded-md bg-neutral-100" />
       </div>
       <div className="flex flex-col gap-2 w-full">
