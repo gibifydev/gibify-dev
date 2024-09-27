07f9bce81812632e145df336c6b6c1a5490c0977
gibify <gibifyofficial@gmail.com>
2024-09-26 21:41:09 -0400

    redes sociais

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,13 +1,13 @@
-import { FaYoutube, FaFacebookF, FaLinkedinIn, FaInstagram } from "react-icons/fa6";
+import { FaFacebookF, FaLinkedinIn, FaInstagram } from "react-icons/fa6";
 
 
 export default function Links() {
   return (
     <div className="flex flex-col items-center md:items-start">
-          <h3 className="text-lg font-bold mb-4">Segue me</h3>
+          <h3 className="text-lg font-bold mb-4">Redes Sociais</h3>
           <div className="flex space-x-4">
             <a
-              href="https://facebook.com"
+              href="https://www.facebook.com/thegibi.dev/"
               className="text-gray-400 hover:text-white transition"
               target="_blank"
               rel="noopener noreferrer"
@@ -15,7 +15,7 @@ export default function Links() {
               <FaFacebookF />
             </a>
             <a
-              href="https://instagram.com"
+              href="https://www.instagram.com/thegibiofficial/"
               className="text-gray-400 hover:text-white transition"
               target="_blank"
               rel="noopener noreferrer"
@@ -23,20 +23,12 @@ export default function Links() {
               <FaInstagram />
             </a>
             <a
-              href="https://twitter.com"
+              href="https://www.linkedin.com/in/thegibiofficial/"
               className="text-gray-400 hover:text-white transition"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaLinkedinIn />
-            </a>
-            <a
-              href="#oi"
-              className="text-gray-400 hover:text-white transition"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaYoutube />
             </a>
           </div>
         </div>
