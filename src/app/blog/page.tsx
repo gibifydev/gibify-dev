f8df520405faea15125d89f659547b701f8e425e
gibify <gibifyofficial@gmail.com>
2024-11-15 12:56:05 -0400

    log: first content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,19 @@
+import path from 'node:path';
+import fs from 'node:fs';
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: 'Blog'
+  title: 'Blog',
+  description: 'As defesas espirituais dentro do castelo de sua mente - Como ENRIQUECER com PAZ DE ESPÍRITO de NAPOLEON HIL',
 }
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const filePath = path.join(process.cwd(), 'public/13f672b5835680738233d85ad5a8b474.html');
+  const htmlContent = fs.readFileSync(filePath, 'utf-8');
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl max-w-5xl px-4">
-        Blog, em breve terá conteúdo
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 max-w-[1024px] mx-auto" >
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
     </main>
   )
 }
