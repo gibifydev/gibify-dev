1b4685b9a909e22a5531d6ac0fc475b08e380db2
gibify <gibifyofficial@gmail.com>
2024-11-16 12:25:05 -0400

    blog list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,5 @@
-import path from 'node:path';
-import fs from 'node:fs';
 import { Metadata } from "next"
+import data from "../../data/articles.json"
 
 export const metadata: Metadata = {
   title: 'Blog',
@@ -8,12 +7,17 @@ export const metadata: Metadata = {
 }
 
 export default async function BlogPage() {
-  const filePath = path.join(process.cwd(), 'public/13f672b5835680738233d85ad5a8b474.html');
-  const htmlContent = fs.readFileSync(filePath, 'utf-8');
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 max-w-[1024px] mx-auto" >
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    <main className="flex min-h-screen flex-col items-center px-4 pt-10 max-w-[1024px] mx-auto" >
+      <h1 className="text-2xl mb-20">Olá 😊! <br />Aqui eu reúno escritos, frases e pensamentos que encontro no meu dia a dia, fruto das minhas pesquisas e leituras sobre a vida.</h1>
+      <ul>
+        {data.articles.map((article) => (
+        <li key={article.id} className="flex flex-col gap-2">
+          <a href={`/blog/${article.id}`} className="border px-6 py-3 rounded-full hover:border-neutral-950">{article.title}</a>
+        </li>
+        ))}
+      </ul>
     </main>
   )
 }
