0575644b4b19169a5f65bf724c0348d8ce285f05
gibify <gibifyofficial@gmail.com>
2024-11-20 14:06:05 -0400

    adjusts general

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -11,10 +11,10 @@ export default async function BlogPage() {
   return (
     <main className="flex min-h-screen flex-col items-center px-4 pt-10 max-w-[1024px] mx-auto" >
       <h1 className="text-2xl mb-20">Olá 😊! <br />Aqui eu reúno escritos, frases e pensamentos que encontro no meu dia a dia, fruto das minhas pesquisas e leituras sobre a vida.</h1>
-      <ul>
+      <ul className="flex flex-col gap-2">
         {data.articles.map((article) => (
-        <li key={article.id} className="flex flex-col gap-2">
-          <a href={`/blog/${article.id}`} className="border px-6 py-3 rounded-full hover:border-neutral-950">{article.title}</a>
+        <li key={article.id} className="border px-6 py-3 rounded-full hover:border-neutral-950">
+          <a href={`/blog/${article.id}`} >{article.title}</a>
         </li>
         ))}
       </ul>
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,85 +1,33 @@
 "use client"
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { FaWhatsapp } from "react-icons/fa";
-import Footer from "../footer";
 
 export default function AboutComponent() {
   return (
-    <section className="h-screen bg-gray-100 py-10">
-    <div className="container mx-auto px-6 lg:px-12">
-        <motion.h1
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-          className="text-4xl md:text-6xl font-bold text-gray-800 mb-4"
-        >
-          Sobre Mim
-        </motion.h1>
-        <motion.p
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.3 }}
-          className="text-lg md:text-2xl text-gray-600 mb-8"
-        >
-          Sou um desenvolvedor apaixonado por tecnologia, focado em criar experiências incríveis com código.
-        </motion.p>
-
-      {/* Foto e Biografia */}
-      <div className="flex flex-col md:flex-row items-center justify-between mt-10">
-        {/* Foto */}
-        <motion.div
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8 }}
-          className="w-full md:w-1/3 mb-10 md:mb-0 md:mr-5"
-        >
-          <Image
-            src="/8S6A6322.jpg"
-            alt="Evandro Gibicoski"
-            width={900}
-            height={900}
-            className="w-full h-auto rounded-lg shadow-lg"
-          />
-        </motion.div>
-
-        {/* Biografia */}
+    <section className="flex flex-col p-8 h-screen bg-gray-100 bg-center bg-cover bg-no-repeat" style={{ backgroundImage: "url('/8S6A6322.jpg')" }}>
+      <div className="flex flex-col md:flex-row items-center justify-between  bg-white/60 rounded-xl p-6">
         <motion.div
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
-          className="w-full md:w-2/3 text-gray-700"
+          className="w-full text-gray-950 font-semibold"
         >
-          <h2 className="text-2xl font-semibold mb-4">Background</h2>
           <p className="mb-4">
-            Tenho mais de 5 anos de experiência de desenvolvimento na indústria de software com especialização em tecnologias como Javascript, CSS, HTML, Typescript, Node.js, Next.js e Electron.js.<br /> Apaixonado por tecnologia, adoro criar aplicações que tenham impacto e ofereçam valor real para os usuários.
+            Tenho mais de 6 anos de experiência de desenvolvimento na indústria de software com especialização em tecnologias como Javascript, CSS, HTML, Typescript, Node.js, Next.js e Electron.js.<br /> Apaixonado por tecnologia, adoro criar aplicações que tenham impacto e ofereçam valor real para os usuários.
           </p>
           <p className="mb-4">
             Sou formado em Gestão da Tecnologia da Informação pela Unicesumar - PR e Pós-Graduado em Engenharia de Software pela Universidade Metropolitana - SP. <br /><br /> Atualmente trabalho como Desenvolvedor de Software Especialista no <a href="https://www.grupoboticario.com.br/" target="_blank" className="text-gray-950 font-semibold">Grupo Boticário</a>.
           </p>
-          <p className="mb-4">
-            Nos meus momentos de lazer, também me aventuro fazendo trilhas, bike e musculação, buscando sempre novas inspirações e formas de se conectar as pessoas através de esportes, além de gostar de música e leituras.
-          </p>
         </motion.div>
       </div>
 
-      {/* Habilidades */}
-      <div className="mt-16">
-        <motion.h2
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-          className="text-3xl font-bold text-center text-gray-800 mb-10"
-        >
-          Skills
-        </motion.h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
           {/* Habilidade 1 */}
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
-            className="bg-white p-6 rounded-lg shadow-lg text-center"
+            className="bg-white/80 p-6 rounded-xl shadow-lg text-center"
           >
             <h3 className="text-xl font-semibold text-gray-800">Frontend</h3>
             <p className="text-gray-600 mt-4">JavaScript, Typescript, CSS, HTML, Next.js, React.js, Node.js</p>
@@ -90,7 +38,7 @@ export default function AboutComponent() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="bg-white p-6 rounded-lg shadow-lg text-center"
+            className="bg-white/80 p-6 rounded-xl shadow-lg text-center"
           >
             <h3 className="text-xl font-semibold text-gray-800">Testes</h3>
             <p className="text-gray-600 mt-4">Jest, Vitest, Cypress, e2e</p>
@@ -101,7 +49,7 @@ export default function AboutComponent() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
-            className="bg-white p-6 rounded-lg shadow-lg text-center"
+            className="bg-white/80 p-6 rounded-xl shadow-lg text-center"
           >
             <h3 className="text-xl font-semibold text-gray-800">UI/UX</h3>
             <p className="text-gray-600 mt-4">TailwindCSS, RadixUI, ShadcnUI, Framer Motion</p>
@@ -112,7 +60,7 @@ export default function AboutComponent() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
-            className="bg-white p-6 rounded-lg shadow-lg text-center"
+            className="bg-white/80 p-6 rounded-xl shadow-lg text-center"
           >
             <h3 className="text-xl font-semibold text-gray-800">Design</h3>
             <p className="text-gray-600 mt-4">Figma</p>
@@ -123,7 +71,7 @@ export default function AboutComponent() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
-            className="bg-white p-6 rounded-lg shadow-lg text-center"
+            className="bg-white/80 p-6 rounded-xl shadow-lg text-center"
           >
             <h3 className="text-xl font-semibold text-gray-800">Desktop</h3>
             <p className="text-gray-600 mt-4">Electron.js, Node.js, WindowsOS, MacOS</p>
@@ -133,30 +81,26 @@ export default function AboutComponent() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
-            className="bg-white p-6 rounded-lg shadow-lg text-center"
+            className="bg-white/80 p-6 rounded-xl shadow-lg text-center"
           >
             <h3 className="text-xl font-semibold text-gray-800">IA</h3>
             <p className="text-gray-600 mt-4">OpenAI, Gemini, LLamma</p>
           </motion.div>
         </div>
-      </div>
 
-      <div className="my-10 text-center">
-        <div className="flex justify-center space-x-6">
-        <motion.a
-      href="https://wa.me/message/QZCLNRIOESI4N1"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8, delay: 0.5 }}
-      className="relative mt-8 px-6 py-3 bg-white text- rounded-full font-bold text-lg shadow-lg hover:shadow-xl transition"
-    >
-      <FaWhatsapp className="inline-block mr-2" size={24}/>
-      Enviar mensagem
-    </motion.a>
-        </div>
-      </div>
-    </div>
-    <Footer />
-  </section>
+          <div className="flex justify-center space-x-6 text-center">
+            <motion.a
+              href="https://api.whatsapp.com/send/?phone=5548998463847"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.5 }}
+              className="my-8 px-6 py-3 bg-gray-950 text-white rounded-xl font-bold text-lg shadow-lg hover:shadow-xl transition w-full max-w-96"
+              target="_blank"
+            >
+              <FaWhatsapp className="inline-block mr-2" size={24}/>
+              Enviar mensagem
+            </motion.a>
+          </div>
+    </section>
   )
 } 
