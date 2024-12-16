189530d5a6f0c8d3c12f4d051aa55779a5801d79
gibify <gibifyofficial@gmail.com>
2024-12-15 20:03:30 -0400

    foi

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,18 +3,23 @@ import data from "../../data/articles.json"
 
 export const metadata: Metadata = {
   title: 'Blog',
-  description: 'As defesas espirituais dentro do castelo de sua mente - Como ENRIQUECER com PAZ DE ESPÍRITO de NAPOLEON HIL',
+  description: 'Aqui eu reúno escritos, frases e pensamentos que encontro no meu dia a dia, fruto das minhas pesquisas e leituras no decorrer da vida.',
 }
 
 export default async function BlogPage() {
+
   return (
     <main className="flex min-h-screen flex-col items-center px-4 pt-10 max-w-[1024px] mx-auto" >
-      <h1 className="text-2xl mb-4">Olá! Eu sou Evandro Gibicoski, o Gibi. Espero que goste o que vai encontrar aqui 😊</h1>
-      <h3 className="text-xl mb-20">Aqui eu reúno escritos, frases e pensamentos que encontro no meu dia a dia, fruto das minhas pesquisas e leituras no decorrer da vida.</h3>
+      <h1 className="text-2xl mb-4">
+        Olá! Eu sou Evandro Gibicoski, o Gibi. Espero que goste o que vai encontrar aqui 😊
+      </h1>
+      <h3 className="text-xl mb-20">
+        Aqui eu reúno escritos, frases e pensamentos que encontro no meu dia a dia, 
+        fruto das minhas pesquisas e leituras no decorrer da vida.</h3>
       <ul className="flex flex-col gap-4">
         {data.articles.map((article) => (
         <li key={article.id} className="border px-6 py-3 rounded-full hover:border-neutral-950 cursor-pointer">
-          <a href={`/blog/${article.id}`} >{article.title}</a>
+          <a href={article.url} target="_blank">{article.title}</a>
         </li>
         ))}
       </ul>
